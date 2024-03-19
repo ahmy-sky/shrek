f56bbbf6f90e474e8bcb180dcfae4611ef1b9d21
ahmy-sky <ahmad.sultan@sky.uk>
2024-03-19 00:15:01 +0000

    messing about with audio

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <audio controls autoPlay className="mt-4">
+          <source src="/shrek-music.mp3" type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </body>
     </html>
   );
 }
